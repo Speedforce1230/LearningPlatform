4aae23280a24e4741c030db1260a9549d675bdf6
Vasu Dhall <gamedhall@gmail.com>
2025-09-03 16:02:41 +0530

    Added some more of the components from my website, more dependencies installed.

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,38 @@
+import styles from "./Slider.module.css";
+import { motion } from "motion/react";
+function Slider({
+    children,
+    initialX = 0,
+    finalX = 0,
+    ref,
+    transition = undefined,
+    drag = false,
+    dragConstraints = false,
+    gap = "0px",
+    onDrag = () => {},
+    onDragEnd = () => {},
+    sliderStyle = {},
+    animateSlider = { x: finalX },
+    onHoverEnd = () => {},
+    onHoverStart = () => {},
+}) {
+    return (
+        <motion.div
+            className={`${styles["slider"]}`}
+            initial={{ x: initialX }}
+            animate={animateSlider}
+            transition={transition}
+            drag={drag}
+            dragConstraints={dragConstraints}
+            style={{ gap, ...sliderStyle }}
+            onDrag={onDrag}
+            onDragEnd={onDragEnd}
+            onHoverEnd={onHoverEnd}
+            onHoverStart={onHoverStart}
+            ref={ref}
+        >
+            {children}
+        </motion.div>
+    );
+}
+export default Slider;
